f1ebe1a7ab8a2ab109f57283f4893d72636e194c
Lisandro Calzada <clisandro1997@gmail.com>
2023-07-06 18:23:55 -0300

    main

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -51,24 +51,4 @@ function cargarPreviewLS(id)
 }
 
 
-function render() {
-    let productos = cargarFiltradoSS();
-    let productRender = '';
-    productos.forEach((producto) => {
-        productRender += `<div class="card m-2" style="width: 17rem;box-shadow: 5px 5px 5px rgb(196, 196, 196) !important;">
-                <img src="${producto.img}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title" style="font-weight:bolder">${producto.modelo}</h5>
-                        <p class="card-text">${producto.tipo}</p>
-                        <p class="card-text">${"$" + producto.precio}</p>
-                        <a href="./preview.html" onclick="cargarPreviewLS(${producto.ID})" class="btn btn-success comprar">comprar</a>
-                        </div>
-            </div>`
-    }
-    )
-    document.getElementById("render").innerHTML = productRender;
-    document.getElementById("render").style.display = "flex";
-    document.getElementById("render").style.flexWrap = "wrap";
-    document.getElementById("render").style.flexDirection = "row";
-}
 
